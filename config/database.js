c85ca73cb00a2042461dff465e6b4223b49aef2c
Marcio Cardoso <megc@megc.pt>
2022-07-06 09:50:48 +0100

    server.js: require @strapi/strapi

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
 module.exports = ({ env }) => ({
+  // defaultConnection: env('DATABASE_CONNECTION_NAME'),
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3307),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3307),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+// const strapi = require("@strapi/strapi");
+// strapi({
+//   autoReload: true,
+//   serveAdminPanel: true,
+// }).start();
+
+// server.js
+// @see https://strapi.io/documentation/v3.x/getting-started/deployment.html#application-configuration
 const strapi = require("@strapi/strapi");
-strapi({
-  autoReload: true,
-  serveAdminPanel: true,
-}).start();
+
+strapi(/* {...} */).start();
