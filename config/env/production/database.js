a488878e45726a2571be10d41532d7c32ea1136e
Marcio Cardoso <megc@megc.pt>
2022-07-29 17:43:04 +0100

    Added new field in DB

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "lxmaxpt_strapi1"),
-      user: env("DATABASE_USERNAME", "lxmaxpt_strapi1"),
-      password: env("DATABASE_PASSWORD", "Wfv03z64%"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
